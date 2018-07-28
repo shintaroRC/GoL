8c01454f111a5d62692021fb3e9f134dfb7165a2
shintaroRC <rightwriting425@gmail.com>
2018-07-29 01:36:31 +0900

    Change the cell-state representation
    Before: 0 or 1 -> After: False or True

--- gol.py
+++ gol.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+
 class game_of_life:
     def __init__(self, width=None, height=None, cells=None):
         self.width = width
         self.height = height
         if cells is None:
-            self.cells = [[0 for x in range(width)] for y in range(height)]
+            self.cells = [[False for x in range(width)] for y in range(height)]
         else:
             self.cells = cells
 
@@ -14,12 +18,12 @@ class game_of_life:
             self.height = len(cells)
 
     def __str__(self):
-        return "\n".join(["".join([str(x) for x in row]) for row in self.cells])
+        return "\n".join(["".join(['■'if x else '□' for x in row]) for row in self.cells])
 
     def update_cells(self):
         width = self.width
         height = self.height
-        new_cells = [[0 for x in range(width)] for y in range(height)]
+        new_cells = [[False for x in range(width)] for y in range(height)]
         for i in range(height):
             for j in range(width):
                 new_cells[i][j] = self._next_cell_state(i, j)
@@ -31,24 +35,34 @@ class game_of_life:
         below = (i+1)%self.height
         left = j-1
         right = (j+1)%self.width
-        return (self.cells[above][left]+self.cells[i][left]+self.cells[below][left]
-                +self.cells[above][j]+self.cells[below][j]
-                +self.cells[above][right]+self.cells[i][right]+self.cells[below][right])
+        list_of_nbhd = [self.cells[above][left], self.cells[i][left],
+                        self.cells[below][left],
+                        self.cells[above][j],
+                        self.cells[below][j],
+                        self.cells[above][right],
+                        self.cells[i][right],
+                        self.cells[below][right]]
+
+        return len(list(filter(lambda x:x, list_of_nbhd)))
 
     def _next_cell_state(self, i, j):
-        if self.cells[i][j]==0:
+        if not self.cells[i][j]:
             if self._num_of_nbhd(i, j)==3:
-                return 1
+                return True
             else:
-                return 0
+                return False
         else:
             if ((self._num_of_nbhd(i, j)==2) or (self._num_of_nbhd(i, j)==3)):
-                return 1
+                return True
             else:
-                return 0
+                return False
 
 if __name__ == "__main__":
-    example_cells = game_of_life(cells=[[0,1,0,0,0],[0,0,1,0,0],[1,1,1,0,0],[0,0,0,0,0],[0,0,0,0,0]])
+    example_cells = game_of_life(cells=[[False,True,False,False,False],
+                                        [False,False,True,False,False],
+                                        [True,True,True,False,False],
+                                        [False,False,False,False,False],
+                                        [False,False,False,False,False]])
     print(example_cells)
     print("")
     example_cells.update_cells()
